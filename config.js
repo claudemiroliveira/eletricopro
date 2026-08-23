49f839a880252c9b3c737d50274f71e410cee495
pwas <131096494+claudemiroliveira@users.noreply.github.com>
2026-08-23 14:38:32 -0300

    Fix syntax error in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.APP_CONFIG = {{"web":{"client_id":"763109598165-rdlr2l76tmkb0kut8ejj02nr67qbo01m.apps.googleusercontent.com",
+window.APP_CONFIG = {{"web":{"client_id":"763109598165-rdlr2l76tmkb0kut8ejj02nr67qbo01m.apps.googleusercontent.com"
 };
