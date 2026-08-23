5aaa60648c60a0f48f71906c1f697325d30f1d47
pwas <131096494+claudemiroliveira@users.noreply.github.com>
2026-08-23 14:36:23 -0300

    Update Google Client ID in config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-window.APP_CONFIG = {
-  googleClientId: "COLE_AQUI_SEU_CLIENT_ID_DO_GOOGLE"
+window.APP_CONFIG = {{"web":{"client_id":"763109598165-rdlr2l76tmkb0kut8ejj02nr67qbo01m.apps.googleusercontent.com",
 };
